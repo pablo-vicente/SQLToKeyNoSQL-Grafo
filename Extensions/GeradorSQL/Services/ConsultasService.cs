31357df06c42002d641146e45f03030d9772e170
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-10-22 16:14:54 -0300

    Extensions

--- Extensions/GeradorSQL/Services/ConsultasService.cs
+++ Extensions/GeradorSQL/Services/ConsultasService.cs
@@ -1,15 +1,27 @@
 ﻿using GeradorSQL.Enums;
+using GeradorSQL.Seeds;
 
 namespace GeradorSQL.Services;
 
 public static class ConsultasService
 {
-    public static async Task GerarAsync(Consulta tipoConsulta, int linhas, StreamWriter streamWriter)
+    public static async Task<FileInfo> GerarAsync(Consulta tipoConsulta, int linhas)
     {
-        await Seeds
-            .SeedsFactory
+        var basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Consultas");
+        if (!Directory.Exists(basePath))
+            Directory.CreateDirectory(basePath);
+        
+        var sufixo = tipoConsulta + "_" + linhas * SeedsFactory.Count(tipoConsulta);
+        var file = new FileInfo(Path.Combine(basePath, $"{sufixo}.sql"));
+    
+        var streamWriter = new StreamWriter(file.FullName);
+        
+        await SeedsFactory
             .GenerateAsync(tipoConsulta, linhas, streamWriter)
             .ConfigureAwait(false);
 
+        await streamWriter.FlushAsync();
+
+        return file;
     }
 }
