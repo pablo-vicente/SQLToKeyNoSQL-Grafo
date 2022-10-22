c5adf2590fc8c876b7b8fa25609843fdce61b92f
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-10-22 16:15:25 -0300

    Analisador de Resultados

--- Extensions/AnaliseResultado/Program.cs
+++ Extensions/AnaliseResultado/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Diagnostics;
-using System.Globalization;
-using System.Net.Http.Headers;
-using System.Text.Json;
 using AnaliseResultado;
+using GeradorSQL.Enums;
+using GeradorSQL.Seeds;
+using GeradorSQL.Services;
+using GeradorSQL.Utils;
 
 #region Casos de Testes
 
@@ -22,96 +23,66 @@ using AnaliseResultado;
 // Instruções com 500 mil itens (8.5 milhões de registros)
 // -----------
 
-//      CREATE
-//      INSERT
-// SELECT
+//      CREATE TABLE
+//      INSERT(N)
+//      SELECT
 //      UPDATE
-//      ALTER TABLE
 //      DELETE
 //      INSERT
-//      DROP TABLE:
+//      ALTER TABLE
+//      DROP TABLE
 
 #endregion
 
-var interacoes = 100;
-var cultura = CultureInfo.CreateSpecificCulture("pt-BR");
+Console.WriteLine("-----------------------------------------------------------------------------------------------------------");
+Console.WriteLine("ANALISADOR CONVERSOR NEO4J BANCO DB_MATCONSTRU");
+Console.WriteLine("-----------------------------------------------------------------------------------------------------------");
+
+var linhas = ConsoleUtils.LerQuantidadeConsultas("CONSULTAS");
+var interacoes = ConsoleUtils.LerQuantidadeConsultas("INTERAÇÕES");
 
 var baseProject = AppDomain.CurrentDomain.BaseDirectory;
 var baseDirect = Path.Combine(baseProject, "SCRIPTS");
 var relatorioName = Path.Combine(baseProject, $"RESULTADOS-{DateTime.Now:yyyy-MM-DD HH-mm-ss}.csv");
 var relatorio = new StreamWriter(relatorioName);
 
-var drop = new FileInfo(Path.Combine(baseDirect, "bd_matConstru_DDL_DROP.sql"));
-var create = new FileInfo(Path.Combine(baseDirect, "bd_matConstru_DDL_CREATE.sql"));
-var alter = new FileInfo(Path.Combine(baseDirect, "bd_matConstru_DDL_ALTER.sql"));
-
+(FileInfo arquivo, string  nome) drop = (new FileInfo(Path.Combine(baseDirect, "bd_matConstru_DDL_DROP.sql")), "DROP");
+var create = (new FileInfo(Path.Combine(baseDirect, "bd_matConstru_DDL_CREATE.sql")), "CREATE");
+var alter = (new FileInfo(Path.Combine(baseDirect, "bd_matConstru_DDL_ALTER.sql")), "ALTER");
+var delete = (new FileInfo(Path.Combine(baseDirect, "bd_matConstru_DML_DELETE.sql")), "DELETE");
+var select = (new FileInfo(Path.Combine(baseDirect, "bd_matConstru_DML_SELECT.sql")), "SELECT");
 
 
 var httpCliente = new HttpClient
 {
-    BaseAddress = new Uri("http://localhost:8080/"),
+    BaseAddress = new Uri("http://localhost:8080/")
 };
 
-await LimparBaseAsync(httpCliente, drop);
+await ResultadosService.LimparBaseAsync(httpCliente, drop.arquivo);
+
+var cenario =
+    $"{linhas.ToString("G", ResultadosService.Cultura)}" +
+    $"({(linhas * SeedsFactory.Count(Consulta.Insert)).ToString("G", ResultadosService.Cultura)})";
+
+Console.WriteLine("-----------------------------------------------------------------------------------------------------------");
+Console.WriteLine("GERANDO CONSULTAS NECESSÁRIAS");
+Console.WriteLine("-----------------------------------------------------------------------------------------------------------");
+
+var insert = (await ConsultasService.GerarAsync(Consulta.Insert, linhas), "INSERT");
+var insertN = (await ConsultasService.GerarAsync(Consulta.InsertN, linhas), "INSERTN");
+var update = (await ConsultasService.GerarAsync(Consulta.Update, linhas), "UPDATE");
 
 for (var execucao = 1; execucao <= interacoes; execucao++)
 {
-    var timeResponse = await RodarScriptAsync(httpCliente, create);
-    await SalvarNoRelatorioAsync(timeResponse, "CREATE", string.Empty, execucao); 
-    await RodarScriptAsync(httpCliente, drop);
+    await ResultadosService.RodarScripSalvartAsync(httpCliente, create, relatorio, cenario, execucao);
+    await ResultadosService.RodarScripSalvartAsync(httpCliente, insertN, relatorio, cenario, execucao);
+    await ResultadosService.RodarScripSalvartAsync(httpCliente, select, relatorio, cenario, execucao);
+    await ResultadosService.RodarScripSalvartAsync(httpCliente, update, relatorio, cenario, execucao);
+    await ResultadosService.RodarScripSalvartAsync(httpCliente, delete, relatorio, cenario, execucao);
+    await ResultadosService.RodarScripSalvartAsync(httpCliente, insert, relatorio, cenario, execucao);
+    await ResultadosService.RodarScripSalvartAsync(httpCliente, alter, relatorio, cenario, execucao);
+    await ResultadosService.RodarScripSalvartAsync(httpCliente, drop, relatorio, cenario, execucao);
 }
 
 await relatorio.FlushAsync();
 Process.Start("explorer.exe", relatorioName);
-
-async Task<TimeResponse> RodarScriptAsync(HttpClient httpClient, FileSystemInfo file)
-{
-    using var multipartFormContent = new MultipartFormDataContent();
-    var fileStreamContent = new StreamContent(new FileStream(file.FullName, FileMode.Open));
-    fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
-    multipartFormContent.Add(fileStreamContent, name: "file", fileName: file.Name);
-    var response = await httpClient.PostAsync("/query-file-sql-script", multipartFormContent);
-    
-    response.EnsureSuccessStatusCode();
-    var readAsStringAsync = await response.Content.ReadAsStringAsync();
-
-    var options = new JsonSerializerOptions
-    {
-        PropertyNameCaseInsensitive = true
-    };
-    
-    return JsonSerializer.Deserialize<Report>(readAsStringAsync, options).TimerResponse;
-}
-
-async Task SalvarNoRelatorioAsync(TimeResponse response, string comando, string cenario, int execucao)
-{
-    await relatorio
-        .WriteLineAsync("COMANDO;" +
-                        "CENARIO;" +
-                        "EXECUCAO;" +
-                        $"{nameof(TimeResponse.TempoCamada)};" +
-                        $"{nameof(TimeResponse.TempoConector)};" +
-                        $"{nameof(TimeResponse.TempoNeo4j)}");
-    
-    await relatorio
-        .WriteLineAsync(
-            $"{comando};" +
-            $"{cenario};" +
-            $"{execucao};" +
-            $"{response.TempoCamada.ToString(cultura)};" +
-            $"{response.TempoConector.ToString(cultura)};" +
-            $"{response.TempoNeo4j.ToString(cultura)}");
-}
-
-async Task LimparBaseAsync(HttpClient httpCliente1, FileInfo fileInfo)
-{
-    try
-    {
-        await RodarScriptAsync(httpCliente1, fileInfo);
-    }
-    catch (Exception ex)
-    {
-        // LIMPEZA DA BASE
-        Console.WriteLine(ex);
-    }
-}
---
+++ Extensions/AnaliseResultado/ResultadosService.cs
@@ -0,0 +1,80 @@
+﻿using System.Globalization;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace AnaliseResultado;
+
+public static class ResultadosService
+{
+    public static readonly CultureInfo Cultura = CultureInfo.CreateSpecificCulture("pt-BR");
+
+    public static async Task RodarScripSalvartAsync(
+        HttpClient httpClient,
+        (FileSystemInfo Arquivo, string Comando) instrucao,
+        TextWriter relatorio,
+        string cenario,
+        int execucao)
+    {
+        var timerResponse = await RodarScriptAsync(httpClient, instrucao.Arquivo);
+        await SalvarNoRelatorioAsync(relatorio, timerResponse, instrucao.Comando, cenario, execucao);
+    }
+
+    private static async Task<TimeResponse> RodarScriptAsync(
+        HttpClient httpClient, FileSystemInfo arquivo)
+    {
+        using var multipartFormContent = new MultipartFormDataContent();
+        var fileStreamContent = new StreamContent(new FileStream(arquivo.FullName, FileMode.Open));
+        fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+        multipartFormContent.Add(fileStreamContent, name: "file", fileName: arquivo.Name);
+        var response = await httpClient.PostAsync("/query-file-sql-script", multipartFormContent);
+        
+        response.EnsureSuccessStatusCode();
+        var readAsStringAsync = await response.Content.ReadAsStringAsync();
+    
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        
+        return JsonSerializer.Deserialize<Report>(readAsStringAsync, options).TimerResponse;
+    }
+    
+    private static async Task SalvarNoRelatorioAsync(
+        TextWriter relatorio, 
+        TimeResponse response, 
+        string comando, 
+        string cenario, 
+        int execucao)
+    {
+        await relatorio
+            .WriteLineAsync("COMANDO;" +
+                            "CENARIO;" +
+                            "EXECUCAO;" +
+                            $"{nameof(TimeResponse.TempoCamada)};" +
+                            $"{nameof(TimeResponse.TempoConector)};" +
+                            $"{nameof(TimeResponse.TempoNeo4j)}");
+        
+        await relatorio
+            .WriteLineAsync(
+                $"{comando};" +
+                $"{cenario};" +
+                $"{execucao};" +
+                $"{response.TempoCamada.ToString(Cultura)};" +
+                $"{response.TempoConector.ToString(Cultura)};" +
+                $"{response.TempoNeo4j.ToString(Cultura)}");
+    }
+    
+    public static async Task LimparBaseAsync(HttpClient httpCliente1, FileSystemInfo fileInfo)
+    {
+        try
+        {
+            await RodarScriptAsync(httpCliente1, fileInfo);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+    
+}
