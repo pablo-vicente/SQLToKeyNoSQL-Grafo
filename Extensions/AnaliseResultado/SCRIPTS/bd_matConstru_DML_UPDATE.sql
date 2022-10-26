b6277ac585cbb686a0bfc1da1c9234b898fbf860
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-10-25 21:37:16 -0300

    Adicionado FKs update

--- Extensions/AnaliseResultado/SCRIPTS/bd_matConstru_DML_UPDATE.sql
+++ Extensions/AnaliseResultado/SCRIPTS/bd_matConstru_DML_UPDATE.sql
@@ -8,13 +8,10 @@ UPDATE Fabricante SET nome_fabricante='Tigre ltda (Atualizado)', marca_fabricant
 UPDATE Setor SET setor_produto='Hidraulica (Atualizado)', sub_setor_produto='valvula', tipo='Premium';
 UPDATE Localizacao_Prod SET Corredor='1a (Atualizado)', partilhera='1a', gaveta='1a';
 UPDATE Forma_pag SET desc_formPag='Dinheiro (Atualizado)';
-UPDATE funcionario SET Nome='Lucas Oliveira (Atualizado)', data_nasc='1998-04-23', telefone='(19)3333-3333', email='Lucas@com.net', t_pessoa='Pessoa Física', razao_social='num sei cara', RG='45.343.942-1', CPf='450.802.308.86', endereco='Rua: vinicius de Moraes n°846', cep='13.188-103', cnh='1111', cat='2222', ctps='3333', bairro='Jd.Amanda', cidade='Hortolândia', uf='sp', agencia='4444', conta='5555';
-UPDATE usuario SET user_log='adm (Atualizado)', user_pwd='adm';
-UPDATE Produto SET cod_barra=12345678, descricao='Torneira (Atualizado)', marca='tigre', unidade_med='unid', preco_compra=10, preco_venda=20, margem_lucro=100;
-UPDATE Item_venda SET quant_vendida=1, prec_unitario=100;
-UPDATE estoque SET quant_disponivel=100, dataADD='2015-07-16 (Atualizado)';
-UPDATE vendas SET valor_venda=1000, valor_recebido=2000, troco=1000, data_venda='2004-05-23T14:25:10', entrega='23/05/1998 (Atualizado)';
-UPDATE entrega SET rua='Rua (Atualizado)', numero='Numero', bairro='bairro', cidade='cidade', uf='uf', cep='cep';
-
-
-
+UPDATE funcionario SET funcao_id=1, banco_id=1, Nome='Lucas Oliveira (Atualizado)', data_nasc='1998-04-23', telefone='(19)3333-3333', email='Lucas@com.net', t_pessoa='Pessoa Física', razao_social='num sei cara', RG='45.343.942-1', CPf='450.802.308.86', endereco='Rua: vinicius de Moraes n°846', cep='13.188-103', cnh='1111', cat='2222', ctps='3333', bairro='Jd.Amanda', cidade='Hortolândia', uf='sp', agencia='4444', conta='5555';
+UPDATE usuario SET func_id=1, user_log='adm (Atualizado)', user_pwd='adm';
+UPDATE Produto SET id_Setor=1, id_fabricante=1, cod_barra=12345678, descricao='Torneira (Atualizado)', marca='tigre', unidade_med='unid', preco_compra=10, preco_venda=20, margem_lucro=100;
+UPDATE Item_venda SET id_produto=1, quant_vendida=1, prec_unitario=100;
+UPDATE estoque SET id_loc=1, for_id=1, id_produto=1, quant_disponivel=100, dataADD='2015-07-16 (Atualizado)';
+UPDATE vendas SET fun_id=1, id_itemVenda=1, id_formaPag=1, valor_venda=1000, valor_recebido=2000, troco=1000, data_venda='2004-05-23T14:25:10', entrega='23/05/1998 (Atualizado)';
+UPDATE entrega SET id_venda=1, id_veiculo=1, rua='Rua (Atualizado)', numero='Numero', bairro='bairro', cidade='cidade', uf='uf', cep='cep';
