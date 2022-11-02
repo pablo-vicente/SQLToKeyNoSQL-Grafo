e2aabf009fc24b4e9d8a391c04165ddc0fda6abe
pablo <pablo.vicente@outlook.com.br>
2022-11-02 14:58:14 -0300

    AJUSTES

---
+++ Site/index.js
@@ -0,0 +1,37 @@
+const baseUrl = "http://127.0.01:8080";
+
+getConnectores();
+
+async function getConnectores() {
+
+    const request = new Request(new URL(baseUrl + "/connectors"),
+        {
+            method: 'GET',
+        });
+
+    await fetch(request)
+        .then(async res => {
+            if (!res.ok) {
+                await handleErro(res)
+                return
+            };
+
+            const connectors = await res.json();
+            const select = document.querySelector("#select-target-nosql");
+
+            select.appendChild("<option value='' selected>Selecione o conector...</option>")
+
+            connectors.forEach(connector => {
+                select.appendChild(`<option value="${connector}">${connector}</option>`)
+            });
+
+
+
+
+        })
+}
+
+async function handleErro(res) {
+    let response = await res.json();
+    console.log(response)
+}
